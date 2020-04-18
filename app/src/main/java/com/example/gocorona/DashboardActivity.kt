9abfb135edd2cf05bef717838fa52bc1dc1cb7f3
maniroopdoulaghar <maniroopdoulaghar@gmail.com>
2020-04-17 23:33:25 -0500

    Designed all the xml's with four modules

--- app/src/main/java/com/example/gocorona/DashboardActivity.kt
+++ app/src/main/java/com/example/gocorona/DashboardActivity.kt
@@ -13,10 +13,9 @@ class DashboardActivity : AppCompatActivity() {
         setContentView(R.layout.activity_dashboard)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
+       /* fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
-        }
+        }*/
     }
-
 }
